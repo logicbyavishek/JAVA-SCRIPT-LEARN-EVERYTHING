bcf0049a3d45fb3ccb49ba6733731ecf01680e9e
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-01 12:57:47 +0530

    NaN-Not a Number

--- Script.js
+++ Script.js
@@ -1,3 +1,8 @@
-// Loose vs Strict Equality
-console.log(5 == '5');  // true (loose equality, type coercion)
-console.log(5 === '5'); // false (strict equality, no type coercion)
+// NaN - Not a Number
+console.log(0/0); // NaN
+console.log(Math.sqrt(-1)); // NaN
+console.log(typeof NaN); // "number"
+console.log(NaN === NaN); // false
+console.log(isNaN(NaN)); // true
+console.log(isNaN("Hello")); // true
+console.log(isNaN(123)); // false
