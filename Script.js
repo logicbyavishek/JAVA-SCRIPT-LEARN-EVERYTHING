2a5f585c464e84946f485d97e036090afd82c19f
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-01 12:32:32 +0530

    let done

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
-var a = 10;
-console.log(a);
-var a = 20 ;
-console.log(a);
+let age =18;
+console.log(age);
+age = 20;
+console.log(age);
+// let age = 22;  -- can be reassigned but cannot be redeclared
