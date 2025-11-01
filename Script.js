80678309bf5b3d31c60a92eeb2c201c33fcfde2a
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-01 13:05:04 +0530

    TRUTHY VS FALSY

--- Script.js
+++ Script.js
@@ -1,8 +1,19 @@
-// NaN - Not a Number
-console.log(0/0); // NaN
-console.log(Math.sqrt(-1)); // NaN
-console.log(typeof NaN); // "number"
-console.log(NaN === NaN); // false
-console.log(isNaN(NaN)); // true
-console.log(isNaN("Hello")); // true
-console.log(isNaN(123)); // false
+//TRUTHY VS FALSY
+// FALSY VALUES
+// 0 "" null undefined NaN Document.all this aare falsy rest all are truthy values
+console.log(Boolean(0)); // false
+console.log(Boolean("")); // false
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(Document.all)); // false
+
+// TRUTHY VALUES
+console.log(Boolean(1)); // true
+console.log(Boolean("Hello")); // true
+console.log(Boolean([])); // true
+console.log(Boolean({})); // true
+console.log(Boolean(-5)); // true
+console.log(Boolean(" ")); // true
+console.log(Boolean(function(){})); // true
+
