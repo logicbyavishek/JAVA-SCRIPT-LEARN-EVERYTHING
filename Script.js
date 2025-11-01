599f2f27ff08eb02b2adf8d35fd72caf89668ee2
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-01 12:42:29 +0530

    Hoisting

--- Script.js
+++ Script.js
@@ -1,8 +1,25 @@
-{
-    var x = 5;
-    let y = 10;
-    const z = 15;
-}
-console.log(x); // 5
-// console.log(y); // ReferenceError: y is not defined
-// console.log(z); // ReferenceError: z is not defined
+// Java Script prepare memory before running code. It moves all declarations to the top this is called hoisting.
+
+// In Java Script var variables are hoisted and initialized with undefined. while let and const are hoisted but not initialized. 
+// So accessing them before declaration will throw ReferenceError.
+
+console.log(a); // undefined
+var a = 10;
+/* Output: undefined
+var a ;
+a = 10;
+
+a is declared but not initialize then var a; this is gop in top of the code , and we are not assign any value to a so it is 
+undefined. so our output is undefined.
+
+let and const hoisting example:
+console.log(b); // ReferenceError: Cannot access 'b' before initialization
+let b = 20;
+/* Output: ReferenceError: Cannot access 'b' before initialization
+Here b is hoisted but not initialized. So accessing it before declaration throws ReferenceError.
+console.log(c); // ReferenceError: Cannot access 'c' before initialization
+const c = 30;
+/* Output: ReferenceError: Cannot access 'c' before initialization
+Here c is hoisted but not initialized. So accessing it before declaration throws ReferenceError.
+
+*/
