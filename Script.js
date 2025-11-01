24a33dffa687d27448142b4030c9f889cf0f901a
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-01 12:47:39 +0530

    Temporal Dead Zone

--- Script.js
+++ Script.js
@@ -1,25 +1,9 @@
-// Java Script prepare memory before running code. It moves all declarations to the top this is called hoisting.
-
-// In Java Script var variables are hoisted and initialized with undefined. while let and const are hoisted but not initialized. 
-// So accessing them before declaration will throw ReferenceError.
-
-console.log(a); // undefined
-var a = 10;
-/* Output: undefined
-var a ;
-a = 10;
-
-a is declared but not initialize then var a; this is gop in top of the code , and we are not assign any value to a so it is 
-undefined. so our output is undefined.
-
-let and const hoisting example:
-console.log(b); // ReferenceError: Cannot access 'b' before initialization
-let b = 20;
-/* Output: ReferenceError: Cannot access 'b' before initialization
-Here b is hoisted but not initialized. So accessing it before declaration throws ReferenceError.
-console.log(c); // ReferenceError: Cannot access 'c' before initialization
-const c = 30;
-/* Output: ReferenceError: Cannot access 'c' before initialization
-Here c is hoisted but not initialized. So accessing it before declaration throws ReferenceError.
-
+// Temporal Dead Zone (TDZ)
+/*
+TDZ means when let and const are used in hoisting concept then rhey are throw error , this is a refference error, because they
+are not initialized. so we can not access them before initialization. the line we initialize them  1st line to the line we 
+initialize them is called their temporal dead zone.
 */
+
+console.log(a); // ReferenceError: Cannot access 'a' before initialization
+let a = 10;
