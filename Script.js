7a067154adf88caa4079f7ef0100e98e831a0d5c
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-01 13:12:31 +0530

    Number.MAX_SAFE_INTEGER

--- Script.js
+++ Script.js
@@ -1,19 +1,4 @@
-//TRUTHY VS FALSY
-// FALSY VALUES
-// 0 "" null undefined NaN Document.all this aare falsy rest all are truthy values
-console.log(Boolean(0)); // false
-console.log(Boolean("")); // false
-console.log(Boolean(null)); // false
-console.log(Boolean(undefined)); // false
-console.log(Boolean(NaN)); // false
-console.log(Boolean(Document.all)); // false
+//Number.MAX_SAFE_INTEGER
+//9007199254740991
 
-// TRUTHY VALUES
-console.log(Boolean(1)); // true
-console.log(Boolean("Hello")); // true
-console.log(Boolean([])); // true
-console.log(Boolean({})); // true
-console.log(Boolean(-5)); // true
-console.log(Boolean(" ")); // true
-console.log(Boolean(function(){})); // true
-
+console.log(Number.MAX_SAFE_INTEGER);
