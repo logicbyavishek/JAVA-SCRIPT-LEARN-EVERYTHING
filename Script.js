1b3380894646f9636b7d0354244a006e27297699
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-02 19:47:10 +0530

    question 7

--- Script.js
+++ Script.js
@@ -1,25 +1,17 @@
-//5. a> print whether numbers from 1 to 10 are even or odd  for each number check :"even", else -> "odd"
-// b> ask user for a number and say if its  positive or negative  use prompt() and a condition
-console.log("<<<<<==============QUESTION NUMBER 5 <A> ============>>>>>");
-for(let i=1 ; i<=10 ; i++){
-    if(i%2===0){
-        console.log(`even number = ${i}`);
-    }
-    console.log(`Odd number = ${i}`);    
-}
-
-console.log("<<<<<==============QUESTION NUMBER 5 <B> ============>>>>>");
+// 7. Ask user’s age and check if eligible to vote
+// If age >= 18 → “Eligible”, else → “Not eligible”
+console.log("<<<<<============== QUESTION NUMBER 6 ============>>>>>");
 
 function checkNumber() {
-    let num = prompt("Enter a number");
-    if (num === null) return console.warn("You pressed Cancel Button Bhai");
-    num = num.trim();
-    if (num === "") return console.warn("Bhai, kuch likhle space mat de!");
-    if (isNaN(num)) return console.warn("Bhai, number de de!");
-    console.log("Confirmed, ye number hai:", Number(num));
-    return Number(num);
+    let age = prompt("Enter Your Age");
+    if (age === null) return console.warn("You pressed Cancel Button Bhai");
+    age = age.trim();
+    if (age === "") return console.warn("Bhai, kuch likhle space mat de!");
+    if (isNaN(age)) return console.warn("Bhai, number de de!");
+    console.log("Confirmed, ye number hai:", Number(age));
+    return Number(age);
 }
 
-let num = checkNumber();
-let msg = num>0?`${num} This is a Possitive Number`:num===0?`${num} you press zero`:`${num} This is a Negative Number`;
+let age = checkNumber();
+let msg = age>18?`${age} Not Eligible`:`${age} Eligible `;
 console.log(msg);
