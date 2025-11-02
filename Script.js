4a56007704d048fd591f327c101d0452dcc73259
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-02 18:34:27 +0530

    question 3

--- Script.js
+++ Script.js
@@ -1,5 +1,4 @@
-//2. print only even numbers from 1 to 20 use loop and conditions to print even ones . 
-
-for(let i =1 ; i<=20 ; i++){
-    if(i%2===0) console.log(i);
+//3. print numbers from 10 to 1 reverse loop with a decrement .
+for(let i=10; i>=1 ; i--){
+    console.log(i);
 }
