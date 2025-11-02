82b91796252b417b799fc2f48acba1bc6a0529fa
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-02 18:38:41 +0530

    question 4

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
-//3. print numbers from 10 to 1 reverse loop with a decrement .
-for(let i=10; i>=1 ; i--){
-    console.log(i);
+//4. print the word "yes" 5 times repeat using a loop
+
+for(let i =1 ; i<=5;i++){
+    console.log("yes");
+    
 }
