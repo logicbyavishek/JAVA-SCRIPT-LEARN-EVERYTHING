06c5f2d9417b792ee794042d80594c543d873fc4
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-02 19:54:35 +0530

    question 9

--- Script.js
+++ Script.js
@@ -1,6 +1,10 @@
-// 8. Print multiplication table of 5
-// Use loop to print 5 × 1 to 5 × 10.
-
-for(let i = 1 ; i <=10 ; i++){
-    console.log(`5 x ${i} = ${5*i}`);
+// 9. Count how many numbers between 1 and 15 are greater than 8
+// Loop and count conditionally.
+let count = 0;
+for(let i = 1 ; i<=15 ; i++){
+    if(i>8){
+        console.log(i);
+        count++;
+    }
 }
+console.log(`${count} numbers are between 1 and 15 greater than 8 .`);
