98dec9713fd47ce01a544280e45f0870c673b9ef
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-03 00:32:38 +0530

    question 13

--- Script.js
+++ Script.js
@@ -27,30 +27,39 @@
 // 12. Ask user for words until they type “stop”. Count how many times they typed “yes”
 // Loop until "stop" is typed. Count "yes".
 
-function checkprompt() {
-    let userInput = prompt("Enter Your Input types Yes - for continue & Stop for stop");
-    if (userInput === null){
-        console.error(" Access not granted -->You pressed Cancel Button Bhai");
-        return null;
-    }
-    if (userInput === ""){
-        console.error("Access not granted -->   You enter nothing ");
-        return "";
-    }
+// function checkprompt() {
+//     let userInput = prompt("Enter Your Input types Yes - for continue & Stop for stop");
+//     if (userInput === null){
+//         console.error(" Access not granted -->You pressed Cancel Button Bhai");
+//         return null;
+//     }
+//     if (userInput === ""){
+//         console.error("Access not granted -->   You enter nothing ");
+//         return "";
+//     }
 
-    return userInput;
+//     return userInput;
+// }
+
+// let count = 0 ;
+
+// while(true){
+//     let userInput = checkprompt();
+//     if(userInput===null) continue;  //early return pattern
+//     if(userInput.toLocaleLowerCase()==="yes"){      //Case-Insensitive Check
+//         count++;
+//     }else if(userInput.toLocaleLowerCase()==="stop"){
+//         console.log(" You type stop and now exist the loop");
+//         break;   // user type "stop" confirm and exit loop
+//     }
+// }
+// console.log(`You types yes ${count} times`);
+
+// 13. Print numbers divisible by 7 from 1 to 50
+// Use modulo % and loop.
+console.log("Print those number divisible by 7 from 1 to 50");
+for(let i = 1 ; i<=50 ; i++){
+    if(i%7===0){
+        console.log(i);
+    }
 }
-
-let count = 0 ;
-
-while(true){
-    let userInput = checkprompt();
-    if(userInput===null) continue;  //early return pattern
-    if(userInput.toLocaleLowerCase()==="yes"){      //Case-Insensitive Check
-        count++;
-    }else if(userInput.toLocaleLowerCase()==="stop"){
-        console.log(" You type stop and now exist the loop");
-        break;   // user type "stop" confirm and exit loop
-    }
-}
-console.log(`You types yes ${count} times`);
