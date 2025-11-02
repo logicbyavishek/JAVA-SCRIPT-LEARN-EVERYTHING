b4bb939caf0b88d042607232579b9987bb131669
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-02 19:50:28 +0530

    question 8

--- Script.js
+++ Script.js
@@ -1,17 +1,6 @@
-// 7. Ask user’s age and check if eligible to vote
-// If age >= 18 → “Eligible”, else → “Not eligible”
-console.log("<<<<<============== QUESTION NUMBER 6 ============>>>>>");
+// 8. Print multiplication table of 5
+// Use loop to print 5 × 1 to 5 × 10.
 
-function checkNumber() {
-    let age = prompt("Enter Your Age");
-    if (age === null) return console.warn("You pressed Cancel Button Bhai");
-    age = age.trim();
-    if (age === "") return console.warn("Bhai, kuch likhle space mat de!");
-    if (isNaN(age)) return console.warn("Bhai, number de de!");
-    console.log("Confirmed, ye number hai:", Number(age));
-    return Number(age);
+for(let i = 1 ; i <=10 ; i++){
+    console.log(`5 x ${i} = ${5*i}`);
 }
-
-let age = checkNumber();
-let msg = age>18?`${age} Not Eligible`:`${age} Eligible `;
-console.log(msg);
