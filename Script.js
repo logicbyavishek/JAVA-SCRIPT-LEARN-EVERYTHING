d9bd9aee75ef1b462bd71ca6ff69d6ea974c3852
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-03 00:42:20 +0530

    question 14

--- Script.js
+++ Script.js
@@ -57,9 +57,20 @@
 
 // 13. Print numbers divisible by 7 from 1 to 50
 // Use modulo % and loop.
-console.log("Print those number divisible by 7 from 1 to 50");
-for(let i = 1 ; i<=50 ; i++){
-    if(i%7===0){
-        console.log(i);
+// console.log("Print those number divisible by 7 from 1 to 50");
+// for(let i = 1 ; i<=50 ; i++){
+//     if(i%7===0){
+//         console.log(i);
+//     }
+// }
+
+// 14. Sum of all odd numbers from 1 to 30
+// Add only odd numbers. Print final sum.
+
+let sum = 0;
+for(let i = 1; i<=30 ; i++){
+    if(i%2!==0){
+        sum+=i;
     }
 }
+console.log(`sum of all odd numbers from 1 to 30 is ${sum}`);
