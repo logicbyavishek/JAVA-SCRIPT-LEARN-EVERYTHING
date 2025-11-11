4d357d2c97efb34ce8b2beb84f71179be6113340
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-12 00:28:31 +0530

    Rest & Default

--- Script.js
+++ Script.js
@@ -1,7 +1,14 @@
 // Default + Rest + Spread
 
-function multiply(a=1,b=1){
-    return a*b;
+// function multiply(a=1,b=1){
+//     return a*b;
+// }
+// multiply();
+
+function sum(...nums){ // Rest
+    return nums.reduce((acc,val)=> acc+val,0);
 }
-multiply();
+
+let nums = [1,2,3];
+console.log(sum(...nums)); //Spread
 
