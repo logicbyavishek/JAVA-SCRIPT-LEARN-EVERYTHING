3ecbbaa307375e3703fdf4fe670ea064250f9eee
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-12 00:47:11 +0530

    IIFE

--- Script.js
+++ Script.js
@@ -1,11 +1,5 @@
-//Closures & Lexical Scope
-function outer(){
-    let count = 0;
-    return function () {
-        count++;
-        console.log(count);
-    }
-}
-let counter = outer();
-counter();
-counter();
+// IIFE - Immediately Invoked Function Expression
+
+(function(){
+    console.log("Hey Avishek this Side");
+})();
