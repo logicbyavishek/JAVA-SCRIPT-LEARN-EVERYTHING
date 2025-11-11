14ae4a08c801955921ce9eb78be104b55a6b80de
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-12 00:33:34 +0530

    First-Class Function

--- Script.js
+++ Script.js
@@ -1,14 +1,10 @@
-// Default + Rest + Spread
-
-// function multiply(a=1,b=1){
-//     return a*b;
-// }
-// multiply();
-
-function sum(...nums){ // Rest
-    return nums.reduce((acc,val)=> acc+val,0);
+// First-Class Function
+function shout(msg){
+    return msg.toUpperCase();
 }
 
-let nums = [1,2,3];
-console.log(sum(...nums)); //Spread
+function ProcessMessage(fn){
+    console.log(fn("hello"));
+}
 
+ProcessMessage(shout);
