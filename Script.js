ce481e6947079020f92e25a456477f20b244e83c
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-13 23:56:34 +0530

    Itetration Methods

--- Script.js
+++ Script.js
@@ -1,6 +1,5 @@
-//Array - Extractors (Don't modify original array)
-let arr = [4,3,2,1,0]
-let newArr = arr.slice(1,3);
-arr.sort();
-console.log(arr);
-console.log(newArr);
+//Array - Itetration Methods
+let prices = [100,200,300];
+let taxed = prices.map(p=>p*1.18)
+console.log(prices);
+console.log(taxed);
