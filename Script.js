dcb18648186c869b0e9036eedca8b8ef76d3ffd8
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-14 00:05:25 +0530

    reduce()

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-//Array - Nums
+//Array - reduce()
 let nums = [1,2,3,4];
-let even = nums.filter(n=> n%2 ===0);
-console.log(even);
+let total = nums.reduce((acc,val)=> acc+val,0);
+console.log(total);
