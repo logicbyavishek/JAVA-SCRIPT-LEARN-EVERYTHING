0d5b2ebdb523b383c26591cf67459bdacb6ea404
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-14 00:00:40 +0530

    Itetration Methods

--- Script.js
+++ Script.js
@@ -1,5 +1,4 @@
-//Array - Itetration Methods
-let prices = [100,200,300];
-let taxed = prices.map(p=>p*1.18)
-console.log(prices);
-console.log(taxed);
+//Array - Nums
+let nums = [1,2,3,4];
+let even = nums.filter(n=> n%2 ===0);
+console.log(even);
