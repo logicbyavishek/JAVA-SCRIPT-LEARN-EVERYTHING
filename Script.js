2b9920ed258e78b3a808f3e09ccff227052acaef
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-14 12:33:55 +0530

    counter make using java Script

--- Script.js
+++ Script.js
@@ -1,4 +1,13 @@
-//Array - Destructuring & Spread
-let nums = [1,2,3,4]
-let [first , second] = ["a","b","c"];
-let newArr = [...nums ,99]
+let h1 = document.getElementById('h1');
+let btn1 = document.getElementById('inc');
+let btn2 = document.getElementById('dec');
+let a = 0;
+btn1.addEventListener('click',function(){
+    a++;
+    h1.innerHTML=a;
+})
+
+btn2.addEventListener('click',function(){
+    a--;
+    h1.innerHTML=a;
+})
