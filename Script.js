613dafa6e255987d18319dc9f3c646259ff6f632
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-17 00:47:12 +0530

    Question 7

--- Script.js
+++ Script.js
@@ -82,12 +82,19 @@
     // console.log(newarr);
 // 6. Use `filter()` to get only even numbers from an array.
 
-    let arr = [1,2,3,4,5,6,7,8,9,10];
-    let newarr = arr.filter(function(val){
-        return val%2===0;
-    })
-    
+    // let arr = [1,2,3,4,5,6,7,8,9,10];
+    // let newarr = arr.filter(function(val){
+    //     return val%2===0;
+    // })
+
 // 7. Use `reduce()` to find the total salary from an array of numbers `[1000, 2000, 3000]`.
+
+    let arr = [1000, 2000, 3000];
+    let ans = arr.reduce(function(accumulator , val){
+        return accumulator + val;
+    },0)
+    console.log(ans);
+    
 // 8. Create an array of names and use `some()` and `every()` to test a condition (e.g., all names longer than 3 chars).
 // 9. Create an object `user` and test the behavior of `Object.freeze()` and `Object.seal()` by adding/changing keys.
 // 10. Create a nested object (`user → address → city`) and access the city name inside it.
