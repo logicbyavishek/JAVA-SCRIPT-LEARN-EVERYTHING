fe6a2872f4d830a07d256f74c6422d3b972610d1
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-17 15:11:51 +0530

    Task 1

--- Script.js
+++ Script.js
@@ -1,7 +1,3 @@
-// What is Dom ? How does it represent the HTML structure ?
-// Name the types of nodes in DOM tree ?
-// whats the difference between element node and text node ?
-// inspect the following HTML structure in the browser and identify each node?
-// what is the difference between document.getElementById and document.querySelector ?
-// what does getElementsByClassName return ? Is it an array ? how to convert it into an array ?
-// use querySelectorAll to secelct buttons with class ".buy-now"
+// T1: Select the heading of a page by ID and changes its text to "Welcome to Sheryians!".
+let heading = document.querySelector("#heading");
+heading.textContent = "Welcome to Sheryians!";
