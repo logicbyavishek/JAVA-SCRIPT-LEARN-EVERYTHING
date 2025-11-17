4828c79df212d15b01fdc728210546b960402379
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-18 01:01:38 +0530

    random notes with disappear effect

--- Script.js
+++ Script.js
@@ -14,16 +14,20 @@ button.addEventListener("click", function(){
     let sc = Math.random()*3;
     let rot = Math.random()*360;
 
-    div.style.height = "10px";
-    div.style.width = "10px";
+    div.style.height = "50px";
+    div.style.width = "50px";
     div.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`;
     div.style.position = "absolute";
     div.style.top = x+"%";
     div.style.left = y+"%";
-    div.style.borderRadius = "50%";
+    // div.style.borderRadius = "50%";
     div.style.transform = `scale(${sc})`;
     div.style.transform = ` rotate(${rot}deg)`;
 
     main.appendChild(div);
+
+    setTimeout(function(){
+        div.remove();
+    },1500);
 
 });
