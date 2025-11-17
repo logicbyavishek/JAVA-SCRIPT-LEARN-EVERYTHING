e9e3a2342c46b1b7703c530909a4e0648e33ef62
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-17 15:55:51 +0530

    Task 5

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
-// remove disabled attribute from a button through JavaScript
-let btn = document.querySelector("button");
-btn.removeAttribute("disabled");
+// create a new list item <li>New Task </li> and add it to end of a <ul>.
+let ul = document.querySelector("ul");
+let li = document.createElement("li");
+li.textContent = "New Task";
+ul.appendChild(li);
