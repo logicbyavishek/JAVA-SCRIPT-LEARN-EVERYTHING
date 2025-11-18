336b0057ad8b054aa84f3f99a3efbe4fb6d809ab
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-18 18:12:55 +0530

    change textContent instead of innerText b/c it's faster

--- Script.js
+++ Script.js
@@ -2,5 +2,5 @@ let select =document.querySelector("select");
 let h1 =document.querySelector("h1");
 
 select.addEventListener("change", function(dets){
-    h1.innerText = `${dets.target.value} Device Selected`;
+    h1.textContent = `${dets.target.value} Device Selected`;
 })
