26c3e9231b49595c2e7d1696956639a015fae73e
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-19 19:56:10 +0530

    Downloader

--- Script.js
+++ Script.js
@@ -1,15 +1,23 @@
-let btn = document.querySelector("#btn");
-let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let h1 = document.querySelector("h1");
+let inner = document.querySelector("#inner");
+let box = document.querySelector("#box");
 
-btn.addEventListener("click",function() {
-    input.click();
-});
-
-input.addEventListener("change",function(dets){
-    let files = dets.target.files[0];
-    if(files!==undefined){
-            btn.textContent = files.name;
-    }else{
-        btn.textContent ="Upload File"
-    }
+let grow = 0;
+btn.addEventListener("click",function(){
+    btn.style.pointerEvents="none";
+    let h4 = document.createElement("h4");
+    let num = (Math.floor(Math.random()*50))+50;
+    let first=setInterval(function() {
+        grow++;
+        h1.textContent=grow+"%";
+        inner.style.width=`${grow}%`;
+    }, num);
+    setTimeout(function() {
+        clearInterval(first);
+        btn.textContent="Downloaded";
+        btn.style.opacity="0.6";
+        h4.innerHTML= `your flie Download sucessfully in <span style="color: blue;font-size: 20px;margin-top:10px;">${num/10}</span> seconds.`;
+        box.appendChild(h4);
+    }, num*100);
 })
