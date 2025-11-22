ec74cb53fed71ffcbcc0f107171c751bbc84c370
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-23 00:05:41 +0530

    add black button on piano

--- Script.js
+++ Script.js
@@ -1,26 +1,27 @@
 const songs = [
-    { id: 0,  src: "songs/24.mp3" },
-    { id: 1,  src: "songs/29.mp3" },
-    { id: 2,  src: "songs/36.mp3" },
-    { id: 3,  src: "songs/41.mp3" },
-    { id: 4,  src: "songs/48.mp3" },
-    { id: 5,  src: "songs/53.mp3" },
-    { id: 6,  src: "songs/60.mp3" },
-    { id: 7,  src: "songs/64.mp3" },
-    { id: 8,  src: "songs/65.mp3" },
-    { id: 9, src: "songs/69.mp3" },
-    { id: 10, src: "songs/72.mp3" },
-    { id: 11, src: "songs/77.mp3" },
-    { id: 12, src: "songs/79.mp3" },
-    { id: 13, src: "songs/84.mp3" },
-    { id: 14, src: "songs/96.mp3" }
+    { id: '0',  src: "songs/24.mp3" },
+    { id: '1',  src: "songs/29.mp3" },
+    { id: '2',  src: "songs/36.mp3" },
+    { id: '3',  src: "songs/41.mp3" },
+    { id: '4',  src: "songs/48.mp3" },
+    { id: '5',  src: "songs/53.mp3" },
+    { id: '6',  src: "songs/60.mp3" },
+    { id: '7',  src: "songs/64.mp3" },
+    { id: '8',  src: "songs/65.mp3" },
+    { id: '9', src: "songs/69.mp3" },
+    { id: 'q', src: "songs/72.mp3" },
+    { id: 'w', src: "songs/77.mp3" },
+    { id: 'e', src: "songs/79.mp3" },
+    { id: 'r', src: "songs/84.mp3" },
+    { id: 't', src: "songs/96.mp3" }
 ];
 
-const keys = document.querySelectorAll(".white");
 
 window.addEventListener("keydown", (e) => {
-    const key = parseInt(e.key);
-    if (key >= 0 && key <= 9) {
+    console.log(e);
+    
+    const key = e.key;
+    if (key=="0"||key=="1"||key=="2"||key=="3"||key=="4"||key=="5"||key=="6"||key=="7"||key=="8"||key=="9"||key=="q"||key=="w"||key=="r"||key=="t"||key=="e") {
         const pressedKey = document.getElementById(key);
         if (pressedKey) {
             pressedKey.classList.add("active");
@@ -30,7 +31,7 @@ window.addEventListener("keydown", (e) => {
 
         if (song) {
             const audio = new Audio(song.src);
-            audio.currentTime = 0;   // every key press starts fresh
+            audio.currentTime = 0;
             audio.play();
         }
     }
