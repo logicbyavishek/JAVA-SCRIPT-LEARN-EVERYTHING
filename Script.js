9c4e4374cc99ca0217978fa6b34c8b30d6775c3d
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-11-27 20:37:34 +0530

    instagram like feature

--- Script.js
+++ Script.js
@@ -1,14 +1,19 @@
-var allBtn = document.querySelectorAll('button')
+var img = document.querySelector('img')
+var love = document.querySelector('#love')
 
-allBtn.forEach(function(elem){
-    elem.addEventListener('click',function(){
-        
-        if(elem.innerHTML == 'Add Friend'){
-            elem.innerHTML = 'Remove'
-        }else{
-            elem.innerHTML = 'Add Friend'
-        }
-        
-    })
-    
+img.addEventListener('dblclick', function () {
+
+    love.style.opacity = 1
+    love.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
+
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%,-300%) scale(1) rotate(60deg)'
+    }, 800)
+    setTimeout(function () {
+        love.style.opacity = 0
+    }, 1000)
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
+    }, 1200)
+
 })
