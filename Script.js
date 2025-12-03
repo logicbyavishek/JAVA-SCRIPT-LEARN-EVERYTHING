d07da802960a1c08c34ab0824a149b27e2e8e867
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-12-03 23:00:15 +0530

    Advance CSS

--- Script.js
+++
@@ -1,7 +0,0 @@
-let box =document.querySelector("#box");
-window.addEventListener('mousemove',function(dets){
-    console.log(dets);
-    
-    box.style.top=dets.clientY+"px";
-    box.style.left=dets.clientX+"px";
-})
