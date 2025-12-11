fdbec35d95ca890a1e5104a6fe95a07e0410127f
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-12-11 22:37:55 +0530

    Matrix text effect

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-addEventListener("mousemove", (e) => {
-    console.log(e.clientX, e.clientY)
-    document.body.style.setProperty("--x", e.clientX + 'px')
-    document.body.style.setProperty("--y", e.clientY + "px")
+const para = document.querySelector('p')
+const text = para.innerText
+const charecter="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+let iteration = 0
+
+function randomText(){
+    const str = text.split("").map((char ,index)=>{
+        if(index<iteration){
+            return char
+        }
+        return charecter.split("")[Math.floor(Math.random()*53)]
+    }).join("")
+    para.innerText = str;
+
+    iteration+=0.25    
+
+    if (iteration >= text.length) {
+        clearInterval(intervalId);
+    }
+}
+
+para.addEventListener('mouseenter',function(){
+    const intervalId = setInterval(randomText,50)
 })
+
