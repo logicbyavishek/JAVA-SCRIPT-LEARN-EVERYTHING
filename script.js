f3928b79292165e84d2d74e3fc54440d09b41440
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2026-01-07 00:34:55 +0530

    call back ex2

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function afterDelay(time , cb){
+function getUser(username , cb){
+    console.log("Fetching user from DB...");
     setTimeout(() => {
-        cb()
-    }, time);
+        cb({id:1223, username: username});
+    }, 1000);
 }
 
-afterDelay(1000, function(){
-    console.log("callback executed");
-});
+function getUserPosts(ussrId,cb){
+    console.log("Fetching posts from DB...");
+    setTimeout(() => {
+        cb(["post1","post2","post3"]);
+    }, 1000);
+}
+
+getUser("Avi",function(obj){
+    console.log("fetching userID .....")
+    getUserPosts(obj.id,function(posts){
+        console.log(posts);
+    });
+})
