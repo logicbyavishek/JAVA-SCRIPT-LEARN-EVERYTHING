36ba62b0828538815cf624bd3cc32ec3bd32e213
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2026-01-10 22:23:21 +0530

    debouncing practice

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-function loginUser(user , cb){
-    console.log("User logging in ...");
-    setTimeout(() => {
-        cb({id: 1, name: user});
-    }, 1000);
-}
-function fetchPermissions(userID , cb){
-    console.log("Fetching user permissions ...");
-    setTimeout(() => {
-        cb(["read", "write", "execute"]);
-    }, 2000);
-}
-function loadDashboard(permissions,cb){
-    console.log("Loading dashboard ...");
-    setTimeout(() => {
-        cb();
-    }, 3000);
-}
+const input = document.getElementById("searchInput");
 
+let timer;
 
-loginUser("Avi",function(obj){
-    fetchPermissions(obj.id,function(permissions){
-        loadDashboard(permissions,function(){
-            console.log("Dashboard loaded with permissions: " + permissions);
-        });
-    });
+input.addEventListener("input", function () {
+  clearTimeout(timer);
+
+  timer = setTimeout(() => {
+    console.log("hello");
+  }, 1000);
 });
-
